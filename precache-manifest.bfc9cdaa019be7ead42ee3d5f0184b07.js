05137f4756710afde3496dfa8f671f7a7990fa04
sukritincs <sukritiverma@northcorp.tech>
2020-08-27 22:52:51 +0530

    Updates

---
+++ precache-manifest.bfc9cdaa019be7ead42ee3d5f0184b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01dc1edc550ef42d077075b8aa391db",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "77760e040301dfe704b2",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a30347523a60f3ef773",
+    "url": "/react-deploy/static/js/2.d427c2e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d427c2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77760e040301dfe704b2",
+    "url": "/react-deploy/static/js/main.1beaf619.chunk.js"
+  },
+  {
+    "revision": "94836679ebefe855ff35",
+    "url": "/react-deploy/static/js/runtime-main.e05b0c96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
